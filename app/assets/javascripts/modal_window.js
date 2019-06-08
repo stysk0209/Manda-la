8604dd75837ded9f7a33a460c7f7241a3560bd77
stysk0209 <stysk0209@outlook.jp>
2019-06-09 02:10:04 +0900

    [Add]ユーザ会員登録,ログイン画面プロトタイプ作成

--- app/assets/javascripts/modal_window.js
+++ app/assets/javascripts/modal_window.js
@@ -1,16 +1,19 @@
 $(function() {
 	$('.modal-open').on('click', function() {
+		// console.log($(this).attr('target'))
+		var modal = $(this).attr('target');
+		// console.log(modal)
 		$(this).blur(); //多重起動防止のため、フォーカスを外す
 		if ($("#modal-overlay")[0]) return false; //2回目以降モーダルを表示しない
 		$('body').append('<div id="modal-overlay"></div>');
 		$('#modal-overlay').fadeIn('slow');
 		// モーダルをセンタリング
-		centeringModelSyncer();
+		centeringModelSyncer(modal);
 		// モーダルをフェードイン
-		$('#modal-content').fadeIn('slow');
+		$(modal).fadeIn('slow');
 		// モーダルを閉じる
 		$('#modal-overlay,#modal-close').on('click', function() {
-			$('#modal-content,#modal-overlay').fadeOut('slow', function() {
+			$(modal + ', #modal-overlay').fadeOut('slow', function() {
 				$('#modal-overlay').remove();
 			});
 		});
@@ -18,17 +21,17 @@ $(function() {
 });
 
 
-function centeringModelSyncer() {
+function centeringModelSyncer(modal) {
 	// 画面の高さと幅を取得して、変数に格納
 	var w = $(window).width();
 	var h = $(window).height();
 	// モーダルの高さと幅を取得して、変数に格納
-	var cw = $('#modal-content').outerWidth();
-	var ch = $('#modal-content').outerHeight();
+	var cw = $(modal).outerWidth();
+	var ch = $(modal).outerHeight();
 	// コンテンツを真ん中に配置するために必要な余白を計算
 	var pxleft = ((w - cw)/2);
 	var pxtop = ((h - ch)/2);
 	// モーダルのcssに余白の設定
-	$('#modal-content').css({'left': pxleft + 'px'});
-	$('#modal-content').css({'top': pxtop + 'px'});
+	$(modal).css({'left': pxleft + 'px'});
+	$(modal).css({'top': pxtop + 'px'});
 }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+# No Method Error undefined local variable or method `resource'の対応で記述
+	def resource_name
+		:user
+	end
+
+	def resource
+		@resource ||= User.new
+	end
+
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]
+	end
+
 end
