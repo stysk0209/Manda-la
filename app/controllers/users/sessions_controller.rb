e7ae9018fa52fa1c26006d6f3321a33c7197009c
Yusuke Yasuo <yasuo.y@dely.jp>
2019-06-10 15:45:26 +0000

    ログイン成功時にroot_pathにリダイレクト、失敗時にエラー文言を出す（出なかった）

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,51 +1,21 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  skip_before_action :verify_authenticity_token, only: [:create]
 
-skip_before_action :verify_authenticity_token, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-      def create
-        # if request.xhr? #XMLHttpRequest(ajax通信)か判定
-          opts = auth_options # opts = {:scope=>:user, :recall=>"users/sessions#xhr_failure"}
-          opts[:recall] = "#{controller_path}#xhr_failure"
-          self.resource = warden.authenticate!(opts)
-          unless sign_in(resource_name, resource)
-
-          end
-          # xhr_success
-          redirect_to root_path
-        # else
-          # @error = "ログイン失敗"
-          # render :template => "mandalas/top"
-        # end
-      end
-
-      def xhr_success
-        render json: { result: true }
-      end
-
-      def xhr_failure
-          @error = "ログイン失敗"
-          render "mandalas/top"
-        # render json: { result: false, errors: ["Login failed."] }
-      end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def create
+    email =  params['user']['email']
+    user = User.find_by(email: email)
+    unless user
+      flash.now[:error] = 'メッセージの送信に失敗しました'
+      render "/mandalas/top" and return
+    end
+    if user.valid_password?(params['user']['password'])
+      sign_in(user)
+      redirect_to root_path
+    else
+      flash.now[:error] = 'メッセージの送信に失敗しました'
+      render "/mandalas/top"
+    end
+  end
 end
