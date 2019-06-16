3451262f317fb999ee90ce7202a92dd0ec40fe07
stysk0209 <stysk0209@outlook.jp>
2019-06-16 20:12:46 +0900

    [Fix]TOPページ&ヘッダーデザイン修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,3 +16,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # JavaScript読み込みのため記述しています。
 Rails.application.config.assets.precompile += %w( modal_window.js )
 Rails.application.config.assets.precompile += %w( mandalas_new.js )
+Rails.application.config.assets.precompile += %w( normalize.scss )
+Rails.application.config.assets.precompile += %w( gnmenu.js )
+Rails.application.config.assets.precompile += %w( classie.js )
+
